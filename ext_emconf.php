9386c3d7b3009cd89e5111e3e07fb9a65e99059f
franzholz <franzholz@735d13b6-9817-0410-8766-e36946ffe9aa>
2005-05-07 06:57:02 +0000

    tt_products 2.0.1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tt_products/trunk@3060 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,9 +2,9 @@
 
 ########################################################################
 # Extension Manager/Repository config file for ext: "tt_products"
-#
-# Auto generated 30-03-2005 16:28
-#
+# 
+# Auto generated 06-05-2005 22:57
+# 
 # Manual updates:
 # Only the data in the array - anything else is removed by next write
 ########################################################################
@@ -35,8 +35,8 @@ $EM_CONF[$_EXTKEY] = Array (
 	'CGLcompliance_note' => '',
 	'private' => 0,
 	'download_password' => '',
-	'version' => '2.0.0',	// Don't modify this! Managed automatically during upload to repository.
-	'_md5_values_when_last_written' => 'a:36:{s:31:"class.tx_ttproducts_wizicon.php";s:4:"78d7";s:12:"ext_icon.gif";s:4:"eb61";s:15:"ext_icon__h.gif";s:4:"d98f";s:16:"ext_icon__ht.gif";s:4:"40c5";s:17:"ext_icon__htu.gif";s:4:"4628";s:16:"ext_icon__hu.gif";s:4:"3b58";s:15:"ext_icon__t.gif";s:4:"04a3";s:16:"ext_icon__tu.gif";s:4:"fcac";s:15:"ext_icon__u.gif";s:4:"ef5a";s:15:"ext_icon__x.gif";s:4:"3998";s:17:"ext_localconf.php";s:4:"82b2";s:14:"ext_tables.php";s:4:"4f51";s:14:"ext_tables.sql";s:4:"7c0a";s:28:"ext_typoscript_constants.txt";s:4:"6dce";s:24:"ext_typoscript_setup.txt";s:4:"cda1";s:13:"locallang.php";s:4:"4486";s:24:"locallang_csh_ttprod.php";s:4:"51ee";s:25:"locallang_csh_ttprodc.php";s:4:"9dea";s:17:"locallang_tca.php";s:4:"4784";s:15:"productlist.gif";s:4:"a6c1";s:7:"tca.php";s:4:"e38e";s:14:"doc/manual.sxw";s:4:"a83c";s:14:"pi/addmemo.png";s:4:"e952";s:13:"pi/basket.gif";s:4:"40b7";s:26:"pi/class.tx_ttproducts.php";s:4:"24b0";s:14:"pi/delmemo.png";s:4:"ffd3";s:19:"pi/payment_DIBS.php";s:4:"bb33";s:29:"pi/payment_DIBS_template.tmpl";s:4:"0eb3";s:32:"pi/payment_DIBS_template_uk.tmpl";s:4:"96f9";s:31:"pi/products_comp_calcScript.inc";s:4:"046b";s:21:"pi/products_help.tmpl";s:4:"7586";s:21:"pi/products_help1.gif";s:4:"46d1";s:20:"pi/products_mail.inc";s:4:"94f2";s:25:"pi/products_template.tmpl";s:4:"efc5";s:28:"pi/products_template_de.tmpl";s:4:"f9f3";s:34:"pi/products_template_htmlmail.tmpl";s:4:"aa8a";}',
+	'version' => '2.0.1',	// Don't modify this! Managed automatically during upload to repository.
+	'_md5_values_when_last_written' => 'a:56:{s:9:"ChangeLog";s:4:"37ab";s:31:"class.tx_ttproducts_wizicon.php";s:4:"78d7";s:12:"ext_icon.gif";s:4:"eb61";s:15:"ext_icon__h.gif";s:4:"d98f";s:16:"ext_icon__ht.gif";s:4:"40c5";s:17:"ext_icon__htu.gif";s:4:"4628";s:16:"ext_icon__hu.gif";s:4:"3b58";s:15:"ext_icon__t.gif";s:4:"04a3";s:16:"ext_icon__tu.gif";s:4:"fcac";s:15:"ext_icon__u.gif";s:4:"ef5a";s:15:"ext_icon__x.gif";s:4:"3998";s:17:"ext_localconf.php";s:4:"82b2";s:14:"ext_tables.php";s:4:"4f51";s:14:"ext_tables.sql";s:4:"9aab";s:28:"ext_typoscript_constants.txt";s:4:"f51c";s:24:"ext_typoscript_setup.txt";s:4:"4cd0";s:13:"locallang.php";s:4:"4486";s:24:"locallang_csh_ttprod.php";s:4:"51ee";s:25:"locallang_csh_ttprodc.php";s:4:"9dea";s:17:"locallang_tca.php";s:4:"088f";s:15:"productlist.gif";s:4:"a6c1";s:7:"tca.php";s:4:"9bfc";s:11:"cvs/Entries";s:4:"2fe0";s:17:"cvs/Entries.Extra";s:4:"585b";s:21:"cvs/Entries.Extra.Old";s:4:"0701";s:15:"cvs/Entries.Old";s:4:"7cea";s:14:"cvs/Repository";s:4:"983c";s:8:"cvs/Root";s:4:"62a3";s:14:"doc/manual.sxw";s:4:"b7cc";s:15:"doc/cvs/Entries";s:4:"b6e5";s:21:"doc/cvs/Entries.Extra";s:4:"9b14";s:25:"doc/cvs/Entries.Extra.Old";s:4:"9b14";s:19:"doc/cvs/Entries.Old";s:4:"e922";s:18:"doc/cvs/Repository";s:4:"38ef";s:12:"doc/cvs/Root";s:4:"62a3";s:14:"pi/addmemo.png";s:4:"e952";s:13:"pi/basket.gif";s:4:"40b7";s:26:"pi/class.tx_ttproducts.php";s:4:"4a21";s:14:"pi/delmemo.png";s:4:"ffd3";s:32:"pi/example_template_bill_de.tmpl";s:4:"4b28";s:19:"pi/payment_DIBS.php";s:4:"bb33";s:29:"pi/payment_DIBS_template.tmpl";s:4:"0eb3";s:32:"pi/payment_DIBS_template_uk.tmpl";s:4:"96f9";s:31:"pi/products_comp_calcScript.inc";s:4:"046b";s:21:"pi/products_help.tmpl";s:4:"7586";s:21:"pi/products_help1.gif";s:4:"46d1";s:20:"pi/products_mail.inc";s:4:"6ea7";s:25:"pi/products_template.tmpl";s:4:"cc88";s:28:"pi/products_template_de.tmpl";s:4:"5d0d";s:34:"pi/products_template_htmlmail.tmpl";s:4:"cf8a";s:14:"pi/cvs/Entries";s:4:"e50c";s:20:"pi/cvs/Entries.Extra";s:4:"9758";s:24:"pi/cvs/Entries.Extra.Old";s:4:"9758";s:18:"pi/cvs/Entries.Old";s:4:"81c9";s:17:"pi/cvs/Repository";s:4:"df22";s:11:"pi/cvs/Root";s:4:"62a3";}',
 );
 
 ?>
