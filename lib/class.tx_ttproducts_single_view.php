3a647382fba53d2ad49ab684f4fa736b21dc80a2
franzholz <franzholz@735d13b6-9817-0410-8766-e36946ffe9aa>
2005-10-28 11:51:19 +0000

    copied from class.tx_ttproducts.php
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tt_products/trunk@3245 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ lib/class.tx_ttproducts_single_view.php
@@ -0,0 +1,244 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 1999-2005 Kasper Skaarhoj (kasperYYYY@typo3.com)
+*  All rights reserved
+*
+*  This script is part of the Typo3 project. The Typo3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*  A copy is found in the textfile GPL.txt and important notices to the license
+*  from the author is found in LICENSE.txt distributed with these scripts.
+*
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+/**
+ * Part of the tt_products (Shopping System) extension.
+ *
+ * view functions
+ *
+ * $Id$
+ *
+ * @author	Kasper Skårhøj <kasperYYYY@typo3.com>
+ * @author	René Fritz <r.fritz@colorcube.de>
+ * @author	Franz Holzinger <kontakt@fholzinger.com>
+ * @author	Klaus Zierer <zierer@pz-systeme.de>
+ * @package TYPO3
+ * @subpackage tt_products
+ *
+ *
+ */
+
+
+require_once (PATH_BE_ttproducts.'lib/class.tx_ttproducts_article_div.php');
+require_once (PATH_BE_ttproducts.'lib/class.tx_ttproducts_page_div.php');
+
+
+
+class tx_ttproducts_single_view {
+	var $conf;
+	var $config;
+	var $uid; 	// product id
+	var $variants; 	// different attributes
+	var $pid_list;	// comma separated list of page ids (pid)
+	var $pid; // basket pid
+	var $tt_products; // element of class tx_table_db
+	var $tt_products_cat; // element of class tx_table_db
+	var $pibase; // reference to object of pibase
+	var $formUrl; // URL of the form
+	var $basketExt;
+
+ 	function init(&$pibase, &$conf, &$config, $uid, $extVars, $pid_list, $tt_products, $tt_products_cat, $pid, &$formUrl, &$basketExt) {
+ 		$this->pibase = $pibase;
+ 		$this->conf = $conf;
+ 		$this->config = $config;
+ 		$this->uid = $uid;
+ 		$this->variants = $extVars;
+ 		$this->pid_list = $pid_list;
+ 		$this->pid = $pid;
+ 		$this->tt_products = $tt_products;
+ 		$this->tt_products_cat = $tt_products_cat;
+ 		$this->formUrl = $formUrl;
+ 		$this->basketExt = $basketExt;
+ 	}
+
+	function printView(&$templateCode, &$pageArray, &$error_code) {
+		global $TSFE;
+		
+		$content = '';
+		$error_code = '';
+
+		$where = 'uid='.intval($this->uid);
+
+		$this->tt_products->enableFields();
+	 	$res = $this->tt_products->exec_SELECTquery('*', $where .' AND pid IN ('.$this->pid_list.')');
+	 	$row = '';
+		if ($this->config['displayCurrentRecord'])	{
+			$row=$this->pibase->cObj->data;
+		} else {
+			$row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res);
+		}
+
+		if ($this->variants) {
+			tx_ttproducts_article_div::getRowFromVariant ($row, $this->variants);
+		}
+		
+		if($row) {
+		 	// $this->uid = intval ($row['uid']); // store the uid for later usage here
+
+				// Get the subpart code
+			$itemFrameTemplate ='';
+			$giftNumberArray = tx_ttproducts_gifts_div::getGiftNumbers ($row['uid'], $this->variants);
+
+			if ($this->config['displayCurrentRecord'])	{
+				$itemFrameTemplate = '###ITEM_SINGLE_DISPLAY_RECORDINSERT###';
+			} else if (count($giftNumberArray)) {
+				$itemFrameTemplate = '###ITEM_SINGLE_DISPLAY_GIFT###';
+			} else if ($row['inStock']==0 && $conf['showProductsNotInStock']) {
+				$itemFrameTemplate = '###ITEM_SINGLE_DISPLAY_NOT_IN_STOCK###';
+			} else {
+				$itemFrameTemplate = '###ITEM_SINGLE_DISPLAY###';
+			}
+			$itemFrameWork = $this->pibase->cObj->getSubpart($templateCode,tx_ttproducts_view_div::spMarker($itemFrameTemplate));
+
+			if (count($giftNumberArray)) {
+				$personDataFrameWork = $this->pibase->cObj->getSubpart($itemFrameWork,'###PERSON_DATA###');
+				// the itemFramework is a smaller part here
+				$itemFrameWork = $this->pibase->cObj->getSubpart($itemFrameWork,'###PRODUCT_DATA###');
+			}
+
+			// set the title of the single view
+			if($conf['substitutePagetitle']== 2) {
+				$TSFE->page['title'] = $row['subtitle'] ? $row['subtitle'] : $row['title'];
+			} elseif ($conf['substitutePagetitle']) {
+				$TSFE->page['title'] = $row['title'];
+			}
+			$pageCatTitle = '';
+			if ($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][TT_PRODUCTS_EXTkey]['pageAsCategory'] == 1) {
+					$pageCatTitle = $pageArray[$row['pid']]['title'].'/';
+			}
+
+			$catTmp = '';
+			if ($row['category']) {
+				$catTmp = $this->tt_products_cat->getCategory($row['category']);
+				$catTmp = $catTmp['title'];
+			}
+			$catTitle = $pageCatTitle.$catTmp;
+
+/*
+				$catTitle= $this->categories[$row['category']]['title'];
+				if ($this->language > 0 && $row['o_datasheet'] != '') {
+					$datasheetFile = $row['o_datasheet'] ;
+				} else  {
+					$datasheetFile = $row['datasheet'] ;
+				}
+*/
+
+			$datasheetFile = $row['datasheet'];
+
+				// Fill marker arrays
+			$wrappedSubpartArray=array();
+			$pid = ( t3lib_div::_GP('backPID') ? t3lib_div::_GP('backPID') : $TSFE->id);
+			$wrappedSubpartArray['###LINK_ITEM###']= array('<a href="'. $this->pibase->pi_getPageLink($pid,'',tx_ttproducts_view_div::getLinkParams()) .'">','</a>');
+
+			if( $datasheetFile == '' )  {
+				$wrappedSubpartArray['###LINK_DATASHEET###']= array('<!--','-->');
+			}  else  {
+				$wrappedSubpartArray['###LINK_DATASHEET###']= array('<a href="uploads/tx_ttproducts/datasheet/'.$datasheetFile.'">','</a>');
+			}
+
+			$item = tx_ttproducts_basket_div::getItem($row);
+			$forminfoArray = array ('###FORM_NAME###' => 'item_'.$this->uid);
+			$markerArray = tx_ttproducts_view_div::getItemMarkerArray ($this->pibase,$item,$this->basketExt, $catTitle,$this->config['limitImageSingle'],'image', $forminfoArray);
+
+			$subpartArray = array();
+
+			$markerArray['###FORM_NAME###']=$forminfoArray['###FORM_NAME###'];
+
+			$markerArray['###FORM_URL###']=$this->formUrl.'&tt_products='.$this->uid ;
+
+			$url = $this->pibase->pi_getPageLink($TSFE->id,'',tx_ttproducts_view_div::getLinkParams()) ; // $this->getLinkUrl('','tt_products');
+
+			$queryPrevPrefix = '';
+			$queryNextPrefix = '';
+			if ($conf['orderByItemNumberSg']) {
+				$queryPrevPrefix = 'itemnumber < '.intval($row['itemnumber']);
+				$queryNextPrefix = 'itemnumber > '.intval($row['itemnumber']);
+			} else {
+				$queryPrevPrefix = 'uid < '.intval($this->uid);
+				$queryNextPrefix = 'uid > '.intval($this->uid);
+			}
+			$queryprev = '';
+			$wherestock = ($conf['showNotinStock'] ? '' : 'AND (inStock <>0) ');
+			$queryprev = $queryPrevPrefix .' AND pid IN ('.$this->pid_list.')'. $wherestock . $this->pibase->cObj->enableFields('tt_products');
+			$resprev = $GLOBALS['TYPO3_DB']->exec_SELECTquery('*', 'tt_products', $queryprev,'','uid');
+
+			if ($rowprev = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($resprev) )
+				$wrappedSubpartArray['###LINK_PREV_SINGLE###']=array('<a href="'.$url.'&tt_products='.$rowprev['uid'].'">','</a>');
+			else
+				$subpartArray['###LINK_PREV_SINGLE###']='';
+
+			$querynext = $queryNextPrefix.' AND pid IN ('.$this->pid_list.')'. $wherestock . $this->pibase->cObj->enableFields('tt_products');
+			$resnext = $GLOBALS['TYPO3_DB']->exec_SELECTquery('*', 'tt_products', $querynext);
+
+			if ($rownext = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($resnext) )
+				$wrappedSubpartArray['###LINK_NEXT_SINGLE###']=array('<a href="'.$url.'&tt_products='.$rownext['uid'].'">','</a>');
+			else
+				$subpartArray['###LINK_NEXT_SINGLE###']='';
+
+			if (trim($row['color']) == '')
+				$subpartArray['###display_variant1###'] = '';
+			if (trim($row['size']) == '')
+				$subpartArray['###display_variant2###'] = '';
+			if (trim($row['accessory']) == '0')
+				$subpartArray['###display_variant3###'] = '';
+			if (trim($row['gradings']) == '')
+				$subpartArray['###display_variant4###'] = '';
+
+				// Substitute
+			$content= $this->pibase->cObj->substituteMarkerArrayCached($itemFrameWork,$markerArray,$subpartArray,$wrappedSubpartArray);
+
+			if ($personDataFrameWork) {
+
+				$subpartArray = array();
+				$wrappedSubpartArray=array();
+				foreach ($giftNumberArray as $k => $giftnumber) {
+					$markerArray = tx_ttproducts_gifts_div::addGiftMarkers ($markerArray, $giftnumber);
+					$markerArray['###FORM_NAME###'] = $forminfoArray['###FORM_NAME###'].'_'.$giftnumber;
+					$markerArray['###FORM_ONSUBMIT###']='return checkParams (document.'.$markerArray['###FORM_NAME###'].')';
+					$markerArray['###FORM_URL###'] = $this->pibase->pi_getPageLink(t3lib_div::_GP('backPID'),'',tx_ttproducts_view_div::getLinkParams('', array('tt_products' => $row['uid'], 'ttp_extvars' => htmlspecialchars($this->variants)))); // $this->getLinkUrl(t3lib_div::_GP('backPID')).'&tt_products='.$row['uid'].'&ttp_extvars='.htmlspecialchars($this->variants);
+
+					$markerArray['###FIELD_NAME###']='ttp_gift[item]['.$row['uid'].']['.$this->variants.']'; // here again, because this is here in ITEM_LIST view
+					$markerArray['###FIELD_QTY###'] = $this->basketExt['gift'][$giftnumber]['item'][$row['uid']][$this->variants];
+
+					$content.=$this->pibase->cObj->substituteMarkerArrayCached($personDataFrameWork,$markerArray,$subpartArray,$wrappedSubpartArray);
+				}
+			}
+			tx_ttproducts_div::setJS($this->pibase, 'email');  // other JavaScript checks can come here
+		} else {
+			$error_code = 'wrong parameter';
+		}
+	return $content;
+	} // print
+}
+
+
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/tt_products/lib/class.tx_ttproducts_single_view.php'])	{
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/tt_products/lib/class.tx_ttproducts_single_view.php']);
+}
+
+
+?>
