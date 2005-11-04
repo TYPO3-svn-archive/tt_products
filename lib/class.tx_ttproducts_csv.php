da27ba0b2cd95c2b4f3958e4d8c1c75116d6677c
franzholz <franzholz@735d13b6-9817-0410-8766-e36946ffe9aa>
2005-11-04 11:56:57 +0000

    generation of CSV files
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tt_products/trunk@3250 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ lib/class.tx_ttproducts_csv.php
@@ -0,0 +1,179 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2005-2005 Franz Holzinger <kontakt@fholzinger.com>
+*  All rights reserved
+*
+*  This script is part of the Typo3 project. The Typo3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*  A copy is found in the textfile GPL.txt and important notices to the license
+*  from the author is found in LICENSE.txt distributed with these scripts.
+*
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+/**
+ * Part of the tt_products (Shopping System) extension.
+ *
+ * functions for the creation of CSV files
+ *
+ * $Id$
+ *
+ * @author	Klaus Zierer <zierer@pz-systeme.de>
+ * @author	Franz Holzinger <kontakt@fholzinger.com>
+ * @package TYPO3
+ * @subpackage tt_products
+ *
+ *
+ */
+
+require_once (PATH_BE_ttproducts.'lib/class.tx_ttproducts_order_div.php');
+require_once (PATH_BE_ttproducts.'lib/class.tx_ttproducts_view_div.php');
+
+
+class tx_ttproducts_csv {
+	var $pibase; // reference to object of pibase
+	var $calculatedArray; // reference to calculated basket array
+	var $itemArray; // reference to the bakset item array
+
+	/**
+	 * Getting all tt_products_cat categories into internal array
+	 */
+	function init(&$pibase, &$itemArray, &$calculatedArray)	{
+		global $TYPO3_DB;
+ 		$this->pibase = &$pibase;
+ 		$this->calculatedArray = &$calculatedArray;
+ 		$this->itemArray = &$itemArray;
+		
+	} // init
+
+
+	function create($csvorderuid, $csvfilepath, &$error_message) {
+		if ($csvfilepath[strlen($csvfilepath)-1] != '/') {
+			$csvfilepath .= '/';
+		}
+		$csvfilepath .= tx_ttproducts_order_div::getOrderNumber($csvorderuid).'.csv';
+		
+		$csvfile = fopen($csvfilepath, 'w');
+		if ($csvfile !== FALSE)
+		{
+			// Generate invoice and delivery address
+			$csvlinehead = '';
+			$csvlineperson = '';
+			$csvlinedelivery = '';
+/* Added Els: 'feusers_uid,' */
+			$infoFields = explode(',','feusers_uid,name,first_name,last_name,address,telephone,fax,email,company,city,zip,state,country,kk_fa,kk_nr,kk_ablauf,kk_pruefcode,agb');
+			while(list(,$fName)=each($infoFields)) {
+				if ($csvlinehead != '') {
+					$csvlinehead .= ';';
+					$csvlineperson .= ';';
+					$csvlinedelivery .= ';';
+				}
+				$csvlinehead .= '"' . $fName . '"';
+				$csvlineperson .= '"' . str_replace('\r\n', '|', $this->personInfo[$fName]) . '"';
+				$csvlinedelivery .= '"' . $this->deliveryInfo[$fName] . '"';
+			}
+
+			// Generate shipping/payment information and delivery note
+			$csvlineshipping = '"' . $this->basketExtra['shipping.']['title'] . '";"' .
+				tx_ttproducts_view_div::priceFormat($this->calculatedArray['priceTax']['shipping']) . '";"' .
+				tx_ttproducts_view_div::priceFormat($this->calculatedArray['priceNoTax']['shipping']) . '"';
+
+			$csvlinepayment = '"' . $this->basketExtra['payment.']['title'] . '";"' .
+				tx_ttproducts_view_div::priceFormat($this->calculatedArray['priceTax']['payment']) . '";"' .
+				tx_ttproducts_view_div::priceFormat($this->calculatedArray['priceNoTax']['payment']) . '"';
+
+			$csvlinedeliverynote = '"'.$this->deliveryInfo['note'].'"';
+			
+			$csvlinedeliverydesireddate = '"'.$this->deliveryInfo['desired_date'].'"';
+
+			// Build field list
+			$csvfields = explode(',', $this->conf['CSVfields']);
+			$csvfieldcount = count($csvfields);
+			for ($a=0;$a<$csvfieldcount;$a++)
+				$csvfields[$a] = trim($csvfields[$a]);
+
+			// Write description header
+			$csvdescr = '"uid";"count";"color";"size";"accessory";"gradings"';
+			reset($csvfields);
+			foreach($csvfields as $csvfield)
+				$csvdescr .= ';"'.$csvfield.'"';
+			if ($this->conf['CSVinOneLine'])
+			{
+				$csvdescr .= '"deliverynote";"desired date";"shipping method";"shipping_price";"shipping_no_tax";"payment method";"payment_price";"payment_no_tax"';
+				$csvdescr .= ';'.$csvlinehead.';'.$csvlinehead;
+			}
+			$csvdescr .= '\n';
+			fwrite($csvfile, $csvdescr);
+
+			// Write ordered product list
+			reset($this->itemArray);
+
+			$infoWritten = false;
+			// loop over all items in the basket indexed by page and itemnumber
+			foreach ($this->itemArray as $pid=>$pidItem) {
+				foreach ($pidItem as $itemnumber=>$actItemArray) {
+					foreach ($actItemArray as $k1=>$actItem) {
+						$variants = explode(';', $actItem['rec']['extVars']);
+						$csvdata = '"'.intval($actItem['rec']['uid']).'";"'.
+									intval($actItem['count']).'";"'.
+									$variants[0].'";"'.
+									$variants[1].'";"'.
+									$variants[2]/100 .'";"'.
+									$variants[3].'"';
+						reset($csvfields);
+						foreach($csvfields as $csvfield) {
+							$csvdata .= ';"'.$actItem['rec'][$csvfield].'"';
+						}
+						if ($this->conf['CSVinOneLine'] && (!$infoWritten))	{
+							$infoWritten = true;
+							$csvdata .= ';'.$csvlinedeliverynote.';'.$csvlinedeliverydesireddate.';'.$csvlineshipping.';'.$csvlinepayment.';'.$csvlineperson.';'.$csvlinedelivery;
+						}
+						$csvdata .= '\n';
+						fwrite($csvfile, $csvdata);
+					}
+				}
+			}
+
+			if (!$this->conf['CSVinOneLine']) {
+				fwrite($csvfile, '\n');
+				fwrite($csvfile, $csvlinehead . '\n');
+				fwrite($csvfile, $csvlineperson . '\n');
+				fwrite($csvfile, $csvlinedelivery . '\n');
+				fwrite($csvfile, '\n');
+				fwrite($csvfile, $csvlinedeliverynote . '\n');
+				fwrite($csvfile, $csvlinedeliverydesireddate . '\n');
+				fwrite($csvfile, $csvlineshipping . '\n');
+				fwrite($csvfile, $csvlinepayment . '\n');
+			}
+
+			fclose($csvfile);
+		}
+		else {
+			$message = $this->pi_getLL('no csv creation');
+			$messageArr =  explode('|', $message);
+			$error_message=$messageArr[0]. $csvfilepath .$messageArr[1];
+		}
+	}
+}
+
+
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/tt_products/lib/class.tx_ttproducts_csv.php'])	{
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/tt_products/lib/class.tx_ttproducts_csv.php']);
+}
+
+
+?>
