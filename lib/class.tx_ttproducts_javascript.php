008ac610f362ca15adbcaac6b894bfccf0d3b16d
franzholz <franzholz@735d13b6-9817-0410-8766-e36946ffe9aa>
2006-03-16 13:47:09 +0000

    new class which holds all JavaScript code
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tt_products/trunk@3344 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ lib/class.tx_ttproducts_javascript.php
@@ -0,0 +1,174 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2006-2006 Franz Holzinger <kontakt@fholzinger.com>
+*  All rights reserved
+*
+*  This script is part of the Typo3 project. The Typo3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*  A copy is found in the textfile GPL.txt and important notices to the license
+*  from the author is found in LICENSE.txt distributed with these scripts.
+*
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+/**
+ * Part of the tt_products (Shopping System) extension.
+ *
+ * div functions
+ *
+ * $Id$
+ *
+ * @author	Franz Holzinger <kontakt@fholzinger.com>
+ * @package TYPO3
+ * @subpackage tt_products
+ *
+ *
+ */
+
+
+class tx_ttproducts_javascript {
+	var $pibase; // reference to object of pibase
+	var $conf;
+	var $config;
+	var $page;
+
+
+	function init(&$pibase, &$conf, &$config, &$page) {
+		$this->pibase = &$pibase;
+		$this->conf = &$conf;
+		$this->config = &$config;
+		$this->page = &$page;
+	}
+
+
+		/**
+		 * Sets JavaScript code in the additionalJavaScript array
+		 *
+		 * @param		string		  $fieldname is the field in the table you want to create a JavaScript for
+		 * @return	  void
+		 * @see
+		 */
+	function set($fieldname, $params='') {
+		global $TSFE;
+		$js = '';
+		$emailArr =  explode('|', $message = $this->pibase->pi_getLL('invalid_email'));
+
+
+		switch ($fieldname) {
+			case 'email' :
+				$js =
+				'function test (eing) {
+					var reg = /@/;
+					var rc = true;
+					if (!reg.exec(eing)) {
+				 		rc = false;
+				 	}
+				 	return rc;
+				}
+	
+				function checkEmail(element) {
+					if (test(element.value)){
+						return (true)
+					}
+	/* Added els5: comma after the invalid address */
+	//				alert("'.$emailArr[0].'\'"+element.value+"'.$emailArr[1].'")
+					alert("'.$emailArr[0].'\'"+element.value+"\''.$emailArr[1].'")
+					return (false)
+				}
+	
+				function checkParams (formObj) {
+					var rc = true;
+					for (var i = 0; i < formObj.length; i++) {
+						if (formObj[i].type == "text") {
+							var email = /email/i;
+							if (email.exec(formObj[i].name)) {
+								rc = checkEmail (formObj[i]);
+							}
+						}
+						if (!rc) {
+							break;
+						}
+					}
+					return rc;
+				}
+				';
+			break;
+			case 'catselect':
+
+				$js .= 'var c = new Array(); // categories
+';
+				if (is_array($params))	{
+					foreach ($params as $k => $row)	{
+						$js .= 'c['.$k.'] = new Array(3);';
+						$js .= 'c['.$k.'][0] = "'.$row['title'].'"; ' ;
+						$js .= 'c['.$k.'][1] = "'.$row['pid'].'"; ' ;
+						$child_category = $row['child_category'];
+						if (is_array($child_category))	{
+							$js .= 'c['.$k.'][2] = new Array('.count($child_category).');';
+							foreach ($child_category as $k1 => $childCat)	{
+								$js .= 'c['.$k.'][2]['.$k1.'] = "'.$childCat.'"; ' ;
+							}
+						} else {
+							$js .= 'c['.$k.'][2] = "0"; ' ;
+						}
+					$js .= '
+';
+					}
+				}
+				$js .=
+	'
+	function fillSelect (select,id) {
+    var sb;
+    var index = select.selectedIndex;
+    var category = select.options[index].value;
+    var subcategories;
+
+    
+	id = "cat" + id;
+	sb = document.getElementById(id);
+    sb.length = 0;
+    subcategories = c[category][2]; 
+    if (typeof(subcategories) == "object") {
+        sb.options[0] = new Option("Anzahl: "+subcategories.length, "A");
+        for (var i=0; i<subcategories.length; ++i) {
+	        sb.options[i] = new Option(c[c[category][2][i]][0], c[category][2][i]);
+        }
+    } else {
+        /* sb.options[0] = new Option(len, "keine Unterkategorie");*/
+        sb.options[0] = new Option("Test - B", "B");
+    }
+    /* sb.options[0] = new Option("Test", "keine Unterkategorie"); */
+	sb.selectedIndex = 0;
+	';
+				$js .= '
+	}
+	';
+
+			break;
+		}
+
+	$TSFE->setJS ($fieldname, $js);
+	} // setJS
+
+}
+
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/tt_products/lib/class.tx_ttproducts_javascript.php'])	{
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/tt_products/lib/class.tx_ttproducts_javascript.php']);
+}
+
+
+?>
