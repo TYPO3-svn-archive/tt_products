bb0e370eef84af632e15e2acd935a88fa7fc3fcd
franzholz <franzholz@735d13b6-9817-0410-8766-e36946ffe9aa>
2006-06-09 12:08:43 +0000

    Class for configuration settings
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tt_products/trunk@3370 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ lib/class.tx_ttproducts_config.php
@@ -0,0 +1,82 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2006-2006 Franz Holzinger <kontakt@fholzinger.com>
+*  All rights reserved
+*
+*  This script is part of the Typo3 project. The Typo3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*  A copy is found in the textfile GPL.txt and important notices to the license
+*  from the author is found in LICENSE.txt distributed with these scripts.
+*
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+/**
+ * Part of the tt_products (Shopping System) extension.
+ *
+ * configuration
+ *
+ * $Id$
+ *
+ * @author  Franz Holzinger <kontakt@fholzinger.com>
+ * @package TYPO3
+ * @subpackage tt_products
+ *
+ *
+ */
+
+
+
+class tx_ttproducts_config {
+	var $conf;
+	var $config;
+
+	/**
+	 * Getting the configurations
+	 */
+	function init(&$conf, &$config) {
+		$this->conf = &$conf;
+		$this->config = &$config;
+	} // init
+
+
+	function &getTableConf ($tablename, $theCode)	{
+
+		if (is_array($this->conf['conf.']) &&
+			is_array($this->conf['conf.'][$tablename.'.'])
+			)	{
+			if (is_array($this->conf['conf.'][$tablename.'.']['ALL.']))	{
+				$tableConf = $this->conf['conf.'][$tablename.'.']['ALL.'];
+			}
+			if (is_array($this->conf['conf.'][$tablename.'.'][$theCode.'.']))	{
+				$tempConf = $this->conf['conf.'][$tablename.'.'][$theCode.'.'];
+				$tableConf = array_merge($tableConf, $tempConf);
+			}
+		}
+		
+		return $tableConf;
+	}
+
+}
+
+
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/tt_products/lib/class.tx_ttproducts_config.php']) {
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/tt_products/lib/class.tx_ttproducts_config.php']);
+}
+
+
+?>
